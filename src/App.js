9d3f05700311fa9d0ddb1ae5ec70f52d8292e0d7
Kevin Bluer <kevin@bluer.com>
2021-06-07 18:30:26 -0500

    signing button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  normalize as normalizeAddress,
+  signTypedData,
+  signTypedData_v4,
+  signTypedDataLegacy,
+} from 'eth-sig-util';
 
 function App() {
+
+  const signMe = () => {
+    console.log(JSON.stringify(signTypedData_v4))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={() => {signMe()}}>sign me</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
