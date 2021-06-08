987e48034069d3570156702d0c69a978dacbb701
Alex <adonesky@gmail.com>
2021-06-08 13:57:52 -0500

    adding connect button, passing setSignature method

--- src/Auth.js
+++ src/Auth.js
@@ -1,17 +1,29 @@
-import {
-  sigUtil 
-} from 'eth-sig-util';
-
-function Auth({ web3, account }) {
-
-  const SignMe = () => {
+function Auth({ web3, account, setSignature }) {
+  const Login = () => {
 
     const msgParams = JSON.stringify({
-    message: {
-            contents: 'Hello World!'
-        }
+      domain: {
+        chainId: 1,
+        name: 'Leaping Lubins',
+        // verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+        version: '1',
+      },
+  
+      message: { contents: 'Login to Leaping Lubins'},
+      primaryType: 'Message',
+      types: {
+        EIP712Domain: [
+          { name: 'name', type: 'string' },
+          { name: 'version', type: 'string' },
+          { name: 'chainId', type: 'uint256' },
+          // { name: 'verifyingContract', type: 'address' },
+        ],
+        // // Refer to PrimaryType
+        Message: [
+          { name: 'contents', type: 'string' },
+        ],
+      },
     });
-
     const from = account;
     const params = [from, msgParams];
     const method = 'eth_signTypedData_v4';
@@ -23,23 +35,20 @@ function Auth({ web3, account }) {
         from,
       },
       function (err, result) {
+        const { result: signature } = result;
         if (err) return console.dir(err);
         if (result.error) {
           alert(result.error.message);
         }
         if (result.error) return console.error('ERROR', result);
         console.log('TYPED SIGNED:' + JSON.stringify(result.result));
-  
-        const recovered = sigUtil.recoverTypedSignature_v4({
-          data: JSON.parse(msgParams),
-          sig: result.result,
-        });
+        setSignature(signature)
       }
     );
   }
 
   return (
-    <button onClick={() => {SignMe()}}>sign me</button>
+    <button onClick={() => {Login()}}>Login</button>
   );
 }
 
